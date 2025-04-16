150308ff8f8a137ccdb7ee39c664eb46d67c2e9b
AlexisChJ <alexchj1917@gmail.com>
2025-04-16 12:24:23 -0600

    update side bar y demas dependencias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Carrusel from "@/components/Carrusel/Carrusel";
 export default function Home() {
   return (
     <div className="w-full flex bg-slate-200">
-      <div>XD</div>
       <Carrusel />
     </div>
   );
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+  } from "@/components/ui/sidebar"
 
 const SideBar = () => {
 
     return (
-        <div>
-            "algo"
-        </div>
+        <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
     );
 };
 
+
 export default SideBar;
+
+
+
+    
+

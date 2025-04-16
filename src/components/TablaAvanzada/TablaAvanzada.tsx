62783d7075cf3f0aef78b82a5660fb7e0f2e389b
AlexisChJ <alexchj1917@gmail.com>
2025-04-16 17:53:17 -0600

    tabla avanzada terminada

--- src/components/TablaAvanzada/TablaAvanzada.tsx
+++ src/components/TablaAvanzada/TablaAvanzada.tsx
@@ -7,31 +7,68 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-  } from "@/components/ui/table"
-  
-const TablaAvanzada = () => {
+} from "@/components/ui/table"
 
+import {
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from "@/components/ui/pagination"
+
+const TablaAvanzada = () => {
     return (
-        <Table>
-        <TableCaption>A list of your recent invoices.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Invoice</TableHead>
-            <TableHead>Status</TableHead>
-            <TableHead>Method</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          <TableRow>
-            <TableCell className="font-medium">INV001</TableCell>
-            <TableCell>Paid</TableCell>
-            <TableCell>Credit Card</TableCell>
-            <TableCell className="text-right">$250.00</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-      
+        <div className="flex flex-col items-center space-y-2">
+            <div className="w-full max-w-4xl rounded-lg overflow-hidden shadow-sm border">
+                <Table className="w-full">
+                    <TableCaption>Faltan datos por deslegar de CLBRI.</TableCaption>
+                    <TableHeader>
+                        <TableRow className="bg-blue-200 text-blue-800">
+                            <TableHead className="w-[100px]">Datos1</TableHead>
+                            <TableHead>Datos2</TableHead>
+                            <TableHead>Datos3</TableHead>
+                            <TableHead>Datos4</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        <TableRow>
+                            <TableCell className="font-medium">PH</TableCell>
+                            <TableCell>PH</TableCell>
+                            <TableCell>PH</TableCell>
+                            <TableCell>PH</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell className="font-medium">PH</TableCell>
+                            <TableCell>PH</TableCell>
+                            <TableCell>PH</TableCell>
+                            <TableCell>PH</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+
+            <Pagination>
+                <PaginationContent>
+                    <PaginationItem>
+                        <PaginationPrevious href="#" />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationLink href="#">1</PaginationLink>
+                        <PaginationLink href="#">2</PaginationLink>
+                        <PaginationLink href="#">3</PaginationLink>
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationEllipsis />
+                    </PaginationItem>
+                    <PaginationItem>
+                        <PaginationNext href="#" />
+                    </PaginationItem>
+                </PaginationContent>
+            </Pagination>
+        </div>
     );
 };
 

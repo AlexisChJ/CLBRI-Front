e919ba9461e25f6074844ed13b7e0c3a7f327bb4
AlexisChJ <alexchj1917@gmail.com>
2025-04-24 16:03:57 -0600

    search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Carrusel from "@/components/Carrusel/Carrusel";
 import DataContainerDashboard from "@/components/DataContainerDashboard/DataContainerDashboard";
+import { Select4Search } from "@/components/Search Bar/searchBar";
 import SideBar from "@/components/SideBar/SideBar";
 import TablaAvanzada from "@/components/TablaAvanzada/TablaAvanzada";
 
@@ -10,7 +11,7 @@ export default function Home() {
       <SideBar children={undefined} />
       <main className="flex-1 p-60">
         <div className="max-w-9xl mx-auto">
-          <TablaAvanzada />
+          <Select4Search/>
           <DataContainerDashboard />
         </div>
       </main>

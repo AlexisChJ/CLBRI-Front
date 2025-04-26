ff09bbcf507b862d23372352fcea07529d4d8b83
AlexisChJ <alexchj1917@gmail.com>
2025-04-25 22:34:07 -0600

    storybook componentes

--- src/components/TablaAvanzada/TablaAvanzada.stories.ts
+++ src/components/TablaAvanzada/TablaAvanzada.stories.ts
@@ -1,1 +1,14 @@
-export { default } from "./TablaAvanzada"
+import type { Meta, StoryObj } from '@storybook/react';
+import TablaAvanzada from './TablaAvanzada'; 
+
+const meta: Meta<typeof TablaAvanzada> = {
+  title: 'Components/TablaAvanzada',
+  component: TablaAvanzada,
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TablaAvanzada>;
+
+export const Default: Story = {};

59c285bc68209cb1e2f327347c98d2f1de32671b
AlexisChJ <alexchj1917@gmail.com>
2025-05-12 19:46:58 -0600

    tabla con mapa pero a la espera de que othon arregle el mapa

--- src/app/distribucionUsuarios/page.tsx
+++ src/app/distribucionUsuarios/page.tsx
@@ -4,6 +4,7 @@ import { NavBar, Notification } from "@/components/NavBar/NavBar"
 import Sidebar from "@/components/SideBar/SideBar"
 import { SearchBar } from "@/components/SearchBar/SearchBar"
 import TablaAvanzada from "@/components/TablaAvanzada/TablaAvanzada"
+import LocationsMap from "@/components/Mapa/mapa"
 
 export default function vistaMapa() {
     const notificaciones: Notification[] = [{ description: "S" }]
@@ -13,7 +14,17 @@ export default function vistaMapa() {
         <div id="tesss" className="p-5 flex flex-col gap-5 w-full h-full pb-0">
             <NavBar title="Usuarios" opts={[]} selected={0} notificaciones={notificaciones} onValueChange={() => {}}/>
             <SearchBar />
-            <TablaAvanzada />
+            <div style={{ display: 'flex', gap: '40px' }}>
+                <TablaAvanzada />
+                <LocationsMap
+                    adminLocation={{
+                    lat: 19.284056,
+                    lng: -99.135333,
+                    title: undefined,
+                    }}
+                    userLocations={[]}
+                />
+            </div>
         </div>
         </Sidebar>
     )

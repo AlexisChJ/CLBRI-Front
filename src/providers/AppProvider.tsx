6409c7f18e0cb1e7e83e152272f96452f0bf54b1
Ángel Orlando VM <A01659000@tec.mx>
2025-05-29 00:10:21 -0600

    Created app context provider for global notifications

---
+++ src/providers/AppProvider.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Notification } from "@/types/Notification";
+import { createContext, useContext, useState } from "react";
+
+interface AppContextProps {
+    notifications: Notification[];
+};
+
+const AppContext = createContext<AppContextProps>({
+    notifications: []
+});
+
+export const AppContextProvider = ({ children } : { children: React.ReactNode }) => {
+    const [notifications] = useState<Notification[]>([]);
+
+    return (
+        <AppContext.Provider value={{ notifications }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const useAppData = () => useContext(AppContext);
